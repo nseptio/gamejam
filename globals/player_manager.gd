8c4eb29fa5dc497a2294a6c135d15714755027b5
Septio Nugroho <septio.nugroho@ui.ac.id>
2025-04-11 20:08:06 +0700

    feat: add global music manager

--- globals/player_manager.gd
+++ globals/player_manager.gd
@@ -5,7 +5,14 @@ var player: Player
 var is_player_spanwed: bool = false
 
 func _ready() -> void:
-	print("PlayerManager ready")
+	if get_tree().current_scene.name == "MainMenu":
+		return
+	if get_tree().current_scene.name == "GameOver":
+		return
+	if get_tree().current_scene.name == "Story":
+		return
+	
+	print(get_tree().current_scene.name)
 	add_player_instance()
 	await get_tree().create_timer(0.5).timeout
 	is_player_spanwed = true
@@ -17,10 +24,16 @@ func add_player_instance() -> void:
 	print("Player added to scene")
 	
 func set_player_position(_new_pos: Vector2) -> void:
+	if player == null:
+		add_player_instance()
+		is_player_spanwed = true
 	print("Setting player position to: ", _new_pos)
 	player.global_position = _new_pos
 
 func set_as_parent(_parent: Node2D) -> void:
+	if player == null:
+		add_player_instance()
+		is_player_spanwed = true
 	if player.get_parent():
 		player.get_parent().remove_child(player)
 	_parent.add_child(player)
