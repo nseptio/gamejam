847ee0d0077fbd4db873df41dbaa4b8f7b778ba7
Septio Nugroho <septio.nugroho@ui.ac.id>
2025-04-11 11:45:50 +0700

    feat: add area level

--- globals/level_manager.gd
+++ globals/level_manager.gd
@@ -1,11 +1,17 @@
 extends Node
-var current_tilemap_bounds: Array[Vector2]
+
 signal tilemap_bounds_changed(bounds: Array[Vector2])
+signal level_load_started
+signal level_loaded
+
+var current_tilemap_bounds: Array[Vector2]
+var target_transition: String
+var position_offset: Vector2
 
 func _ready() -> void:
-	print("LevelManager ready")
-	# Print the number of connections to our signal
-	print("Initial signal connections: ", get_signal_connection_list("tilemap_bounds_changed").size())
+	await get_tree().process_frame
+	level_loaded.emit()
+
 
 func change_tilemap_bounds(bounds: Array[Vector2]) -> void:
 	print("LevelManager: Changing Camera Bounds: ", bounds)
@@ -13,3 +19,27 @@ func change_tilemap_bounds(bounds: Array[Vector2]) -> void:
 	print("Connections before emit: ", get_signal_connection_list("tilemap_bounds_changed").size())
 	tilemap_bounds_changed.emit(bounds)
 	print("Signal emitted")
+
+func load_new_level(
+	level_path: String,
+	_target_transition: String,
+	_position_offset: Vector2
+) -> void:
+	
+	print("Starting load new level...")
+	get_tree().paused = true
+	target_transition = _target_transition
+	position_offset = _position_offset
+	
+	await SceneTransition.fade_out()
+	level_load_started.emit()
+	
+	await get_tree().process_frame
+	get_tree().change_scene_to_file(level_path)
+	print("Set level to: ", level_path)
+	
+	await SceneTransition.fade_in()
+	get_tree().paused = false
+	
+	await get_tree().process_frame
+	level_loaded.emit()
